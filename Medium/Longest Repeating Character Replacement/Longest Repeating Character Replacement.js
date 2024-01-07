95a5fb1ca33022812388137804c589a54ff5c93d
TheGiantKiller <ktsdsd94@gmail.com>
2024-01-07 16:56:11 +0900

    solve : Longest Repeating Character Replacement

---
+++ Medium/Longest Repeating Character Replacement/Longest Repeating Character Replacement.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let obj={}
+    let maxAlpha=0
+    let left=0
+    let answer=0
+    for(let right=0;right<s.length;right++){
+        if(!obj[s[right]]){
+            obj[s[right]]=1
+        }
+        else{
+            obj[s[right]]+=1
+        }
+        maxAlpha=Math.max(maxAlpha,obj[s[right]])
+        let windowSize=(right-left+1)-maxAlpha
+        if(windowSize>k){
+            obj[s[left]]-=1
+            left+=1
+        }
+        answer=Math.max(right-left+1,answer)
+        
+    }
+    return answer
+    
+};
+
