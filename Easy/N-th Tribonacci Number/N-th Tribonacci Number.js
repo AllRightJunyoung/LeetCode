86a0c669c162074b1d49aacd0d6b67a8a66c71be
TheGiantKiller <ktsdsd94@gmail.com>
2024-02-01 21:54:06 +0900

    solve : N-th Tribonacci Number

---
+++ Easy/N-th Tribonacci Number/N-th Tribonacci Number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    let dp=new Array(38).fill(0)
+    dp[0]=0
+    dp[1]=1
+    dp[2]=1
+    for(let i=3;i<=n;i++){
+     dp[i]=dp[i-3]+dp[i-2]+dp[i-1]
+    }
+    let answer=dp[n]
+    return answer
+ 
+ 
+ };
+ 
+ tribonacci(4)
+ tribonacci(25)
--- test.js
+++ test.js
@@ -1,34 +1,20 @@
 /**
- * @param {number[]} coins
- * @param {number} amount
+ * @param {number} n
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    let dp=new Array(amount+1).fill(Infinity)
-    for(let i=0;i<coins.length;i++){
-        const coin=coins[i]
-        dp[coin]=1
-    }
+var tribonacci = function(n) {
+   let dp=new Array(38).fill(0)
+   dp[0]=0
+   dp[1]=1
+   dp[2]=1
+   for(let i=3;i<=n;i++){
+    dp[i]=dp[i-3]+dp[i-2]+dp[i-1]
+   }
+   let answer=dp[n]
+   return answer
 
-    coins.sort((a,b)=>a-b)
-    dp[0]=0
-    for(let i=1;i<=amount;i++){
-        for(let j=0;j<coins.length;j++){
-            let coin=coins[j]
-            if(i<=coin)break
-        dp[i]=Math.min(dp[i],dp[i-coin]+1)
-        // 현재 amount - 가지고있는 코인 
-        }
-    }
-    let ans;
-    if(dp[amount]===Infinity){
-        ans=-1
-    }
-    else{
-        ans=dp[amount]
-    }
-    return ans
+
 };
-coinChange([1,2,5],100)
-// coinChange([2],3)
-coinChange([1],0)
+
+tribonacci(4)
+tribonacci(25)
