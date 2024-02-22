34128a8526ba603c415a41484ce2fa8a7e8f3c28
AllRightJunyoung <ktsdsd94@gmail.com>
2024-02-22 16:24:31 +0900

    test commit

--- test.js
+++ test.js
@@ -1,28 +1,46 @@
 /**
  * @param {number[]} nums
- * @return {number[]}
+ * @return {number}
  */
-var rearrangeArray = function(nums) {
-    let postive=0
-    let negative=1
 
-    let answer=new Array(nums.length).fill(0)
+//https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/?envType=daily-question&envId=2024-02-15
+var largestPerimeter = function(nums) {
 
-    for(let i=0;i<nums.length;i++){
-        let number=nums[i]
-        if(number>0){
-            answer[postive]=number
-            postive+=2
+    if(nums.length<3){
+        return -1
+    }
+
+    nums.sort((a,b)=>a-b)
+    let prefix=new Array(nums.length).fill(0)
+    prefix[0]=nums[0]
+    for(let i=1;i<nums.length;i++){
+        prefix[i]=prefix[i-1]+nums[i]
+    }
+    let [left,right]=[0,1]
+
+
+
+    let answer=-1
+    let total=prefix[right]
+    while(left<right){
+        if(total>nums[right]){
+            answer=Math.max(answer,total)
+            total+=nums[right]
+            right+=1            
         }
         else{
-            answer[negative]=number
-            negative+=2
+            total-=nums[left]
+            left+=1            
         }
+
 
     }
 
     return answer
+
+
 };
 
-rearrangeArray([3,1,-2,-5,2,-4])
-// rearrangeArray([-1,1])
+// largestPerimeter([5,5,5])
+// largestPerimeter([1,12,1,2,5,50,3])
+largestPerimeter([5,5,50])
