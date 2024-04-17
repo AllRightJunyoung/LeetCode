1c223a511956ebc4b994566d3c01426a507c1377
AllRightJunyoung <ktsdsd94@gmail.com>
2024-04-17 18:37:39 +0900

    solve : Find Eventual Safe States (다시)

--- Medium/Find Eventual Safe States (다시)/Find Eventual Safe States.js
+++ Medium/Find Eventual Safe States (다시)/Find Eventual Safe States.js
@@ -2,53 +2,35 @@
  * @param {number[][]} graph
  * @return {number[]}
  */
-let isCycle=false
+
+// 싸이클 찾기
 var eventualSafeNodes = function(graph) {
     let answer=[]
-    let nodes=new Array(graph.length).fill(0)
+
+    const map=new Map()
     for(let i=0;i<graph.length;i++){
-        nodes[i]=new Array()
-    }
-    for(let i=0;i<graph.length;i++){
-        for(let j=0;j<graph[i].length;j++){
-            nodes[i].push(graph[i][j])
-        }
-    }
-    for(let i=0;i<nodes.length;i++){
-        if(!nodes[i].length){
+        if(dfs(graph,i,map)){
             answer.push(i)
         }
     }
 
-    for(let i=0;i<nodes.length;i++){
-        isCycle=false
-        if(answer.includes(i))continue
-        let visited=new Array(graph.length).fill(false)
-        let flag=dfs(i,nodes,visited,answer,i)
-        if(flag){
-            answer.push(i)
-        }
-    }
-    answer.sort((a,b)=>a-b)
+
+
     return answer
 };
-function dfs(cur,nodes,visited,answer,startNode){
-    if(answer.includes(cur) && !isCycle){
-        return true
-    }
-    visited[cur]=true
-    for(let i=0;i<nodes[cur].length;i++){
-        const next=nodes[cur][i]
-        if(!visited[next]){
-            let flag=dfs(next,nodes,visited,answer,startNode)
-            if(flag)return true
-        }
-        else if(visited[next] || startNode===next){
-            isCycle=true
+function dfs(graph,node,map){
+    if(map.has(node)) return map.get(node)
+   
+    map.set(node,false)
+
+    for(let n of graph[node]){
+        if(!dfs(graph,n,map)){
             return false
         }
     }
-    return false
+    map.set(node,true)
+    return true
 }
+console.log(eventualSafeNodes([[],[0,2,3,4],[3],[4],[]]    ))
 console.log(eventualSafeNodes([[],[0,2,3,4],[3],[4],[]]))
 // console.log(eventualSafeNodes([[0],[2,3,4],[3,4],[0,4],[]]))
